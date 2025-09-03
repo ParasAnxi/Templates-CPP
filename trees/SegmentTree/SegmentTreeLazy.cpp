5a62fef2ff9f87766d02e084f44811d927155d2a
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-09-03 09:46:12 +0530

    st lazy

---
+++ trees/SegmentTree/SegmentTreeLazy.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umcvi = unordered_map<char, vi>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MAX_N 200001
+#define MAX_L 18
+#define MOD 1000000007
+#define INF 1000000000LL
+#define LMOD 1000000000000000007LL
+
+class SegmentTree{
+    public:
+
+    int n;
+    vi lazy, tree, arr;
+
+    void build(int node, int left, int right){
+        if(left == right){
+            tree[node] = arr[left];
+            return;
+        }
+        int mid = (left + right) / 2;
+        build(2 * node, left , mid);
+        build(2 * node + 1, mid + 1, right);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+
+    void push(int node, int left, int right){
+        if(lazy[node] != 0){
+            tree[node] += (right - left + 1) * lazy[node];
+            if(left != right){
+                lazy[2 * node] += lazy[node];
+                lazy[2 * node + 1] += lazy[node];
+            } 
+            lazy[node] = 0;
+        }
+    }
+
+    void rangeUpdate(int node, int left, int right, int l, int r,int val){
+        push(node, left, right);
+        if(right < l || r < left){
+            return;
+        }
+        if(l <= left && right <= r){
+            lazy[node] += val;
+            push(node, left, right);
+            return;
+        }
+        int mid = (left + right) / 2;
+        rangeUpdate(2 * node, left, mid, l, r, val);
+        rangeUpdate(2 * node + 1, mid + 1, right, l , r, val);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+    int qry(int node,int left, int right, int l, int r){
+        push(node, left, right);
+        if(r < left || right < l){
+            return 0;
+        }
+        if(l <= left && right <= r){
+            return tree[node];
+        }
+        int mid = (left + right) / 2;
+        return qry(2 * node, left, mid, l, r) + qry(2 * node + 1, mid + 1, right, l , r);
+    }
+    void solve(){
+        cin >> n;
+        tree.assign(4 * n, 0);
+        lazy.assign(4 * n , 0);
+        arr.assign(n, 0);
+        for(int i = 0 ; i < n ; i++){
+            int x; cin >> x;
+            arr[i] = x;
+        }
+        build(1, 0 , n - 1);
+        cout<< qry(1, 0, n - 1, 0 , n - 1) << "\n";
+        rangeUpdate(1, 0, n - 1, 0, 2, 3);
+        cout<< qry(1, 0, n - 1, 0 , n - 1) << "\n";
+    }
+};
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    SegmentTree st;
+    // int t;
+    // cin >> t;
+    // while (t--) {
+    //     solve();
+    // }
+    st.solve();
+    return 0;
+}
